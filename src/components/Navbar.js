1a8f2ee020d40598211e2f6f150d462adc172644
sriSaksham <sri.saksham78@gmail.com>
2024-08-12 18:18:21 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,59 +1,9 @@
-// import React from 'react';
-// import './Navbar.css';
-// import { Link } from 'react-router-dom';
-
-// const Navbar = () => {
-//   return (
-//     <nav className="navbar">
-//       <Link to="/" className="navbar-logo">Testportal</Link>
-//       <div className="nav-links">
-//         <div className="dropdown">
-//           <button className="dropbtn">Who it's for</button>
-//           <div className="dropdown-content">
-//             <Link to="/">For Beginners</Link>
-//             <Link to="/">For Graduates</Link>
-//             <Link to="/">For Students</Link>
-//           </div>
-//         </div>
-//         <div className="dropdown">
-//           <button className="dropbtn">Test your prep</button>
-//           <div className="dropdown-content">
-//             <Link to="/">Test 1</Link>
-//             <Link to="/">Test 2</Link>
-//             <Link to="/">Test 3</Link>
-//           </div>
-//         </div>
-//         <Link to="/pricing" className="nav-link">Pricing</Link>
-//         <Link to="/resources" className="nav-link">Resources</Link>
-//       </div>
-//       <div className="nav-auth">
-//         <Link to="/login" className="nav-link">Login</Link>
-//         <Link to="/register" className="nav-link signup">Sign up</Link>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
-
 import React from 'react';
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
 
 const CustomNavbar = () => {
-  const navigate = useNavigate();
-
-  const handleTestRedirect = (testPath) => {
-    const token = localStorage.getItem('authToken');
-    if (!token) {
-      navigate('/register');
-    } else {
-      navigate(testPath);
-    }
-  };
-
   return (
     <Navbar bg="dark" variant="dark" expand="lg" fixed="top" className="custom-navbar">
       <Container>
@@ -69,9 +19,10 @@ const CustomNavbar = () => {
               <NavDropdown.Item as={Link} to="/">For Students</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Test your prep" id="test-prep-dropdown">
-              <NavDropdown.Item onClick={() => handleTestRedirect('/test1')}>Test 1</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => handleTestRedirect('/test2')}>Test 2</NavDropdown.Item>
-              <NavDropdown.Item onClick={() => handleTestRedirect('/test3')}>Test 3</NavDropdown.Item>
+              {/* Direct links to test pages without authentication check */}
+              <NavDropdown.Item as={Link} to="/test1">Test 1</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/test2">Test 2</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/test3">Test 3</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link as={Link} to="/pricing">Pricing</Nav.Link>
             <Nav.Link as={Link} to="/resources">Resources</Nav.Link>
