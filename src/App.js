692614ba1bcf3031bc75769460755d6d72d506fe
sriSaksham <sri.saksham78@gmail.com>
2024-08-16 11:35:21 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -1,88 +1,3 @@
-// import React from 'react';
-// import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// import Home from './components/Home';
-// import Mocks from './components/Mocks';
-// import Navbar from './components/Navbar';
-// import Login from './components/Login';
-// import Register from './components/Register';
-// import TestPage from './components/TestPage';
-// import ThankYouPage from './components/ThankYouPage';
-// import MembershipPlans from './components/MembershipPlans';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route path="/mocks" element={<Mocks />} />
-//         <Route path="/test/:testId" element={<TestPage />} />
-//         <Route path="/thank-you" element={<ThankYouPage />} />
-//         <Route path="/membership-plans" element={<MembershipPlans />} />
-//       </Routes>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// import React, { useState, useEffect } from 'react';
-// import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// import Home from './components/Home';
-// import Mocks from './components/Mocks';
-// import Navbar from './components/Navbar';
-// import Login from './components/Login';
-// import Register from './components/Register';
-// import MembershipPlans from './components/MembershipPlans';
-// import TestPage from './components/TestPage';
-// import ForgotPassword from './components/ForgotPassword';
-// import authService from './services/authService';
-
-// function App() {
-//   const [isAuthenticated, setIsAuthenticated] = useState(false);
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     const user = authService.getCurrentUser();
-//     if (user) {
-//       setIsAuthenticated(true);
-//     }
-//   }, []);
-
-//   const handleLogout = () => {
-//     authService.logout();
-//     setIsAuthenticated(false);
-//     navigate('/login');
-//   };
-
-//   return (
-//     <div className="App">
-//       <Navbar isAuthenticated={isAuthenticated} onLogout={handleLogout} />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route path="/mocks" element={<Mocks />} />
-//         <Route path="/membership-plans" element={<MembershipPlans />} />
-//         <Route path="/forgot-password" element={<ForgotPassword />} />
-//         <Route path="/test/:id" element={<PrivateRoute component={TestPage} />} />
-//         <Route path="*" element={<Navigate to="/" />} />
-//       </Routes>
-//     </div>
-//   );
-// }
-
-// function PrivateRoute({ component: Component, ...rest }) {
-//   const user = authService.getCurrentUser();
-//   return user ? <Component {...rest} /> : <Navigate to="/login" replace />;
-// }
-
 // export default App;
 
 
@@ -152,6 +67,7 @@ import TestPage from './components/TestPage';
 import ThankYouPage from './components/ThankYouPage';
 import MembershipPlans from './components/MembershipPlans';
 import ForgotPassword from './components/ForgotPassword';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -162,7 +78,29 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/mocks" element={<Mocks />} />
+
+          {/* Protect these routes */}
+          <Route 
+            path="/mocks" 
+            element={
+              <ProtectedRoute>
+                <Mocks />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/test/:testId" 
+            element={
+              <ProtectedRoute>
+                <TestPage />
+              </ProtectedRoute>
+            } 
+          />
+
+
+
+          {/* <Route path="/mocks" element={<Mocks />} /> */}
+
           <Route path="/test/:testId" element={<TestPage />} />
           <Route path="/thank-you" element={<ThankYouPage />} />
           <Route path="/membership-plans" element={<MembershipPlans />} />
