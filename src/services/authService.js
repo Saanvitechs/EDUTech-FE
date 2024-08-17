797cc780d1a0fde1a6a84c4e7d0fe85827db605b
sriSaksham <sri.saksham78@gmail.com>
2024-08-18 01:46:37 +0530

    18 aug

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,84 +1,3 @@
-// import axios from 'axios';
-
-// const API_URL = 'http://localhost:8080/api/auth/';
-
-// const axiosWithAuth = () => {
-//   const token = localStorage.getItem('token');
-//   return axios.create({
-//     baseURL: API_URL,
-//     headers: {
-//       Authorization: `${token}`,
-//     },
-//   });
-// };
-
-// const register = async (firstName, lastName, username, email, password) => {
-//   try {
-//     const response = await axios.post(API_URL + 'register', {
-//       firstName,
-//       lastName,
-//       username,
-//       email,
-//       password,
-//     });
-//     return response.data;
-//   } catch (error) {
-//     console.error('Registration failed:', error);
-//     throw error;
-//   }
-// };
-
-// const login = async (username, password) => {
-//   try {
-//     const response = await axios.post(API_URL + 'login', {
-//       username,
-//       password,
-//     });
-    
-    
-//     // Extracting the token from the response
-//     if (response.data && response.data.accessToken) {
-//       const token = response.data.accessToken.split(' ')[1]; // Removes the 'Bearer' prefix
-//       localStorage.setItem('token', token);
-//       return response.data;
-//     } else {
-//       throw new Error('Login failed: No token received');
-//     }
-//   } catch (error) {
-//     console.error('Login failed:', error);
-//     throw error;
-//   }
-// };
-
-// const logout = () => {
-//   localStorage.removeItem('token');
-// };
-
-// const getCurrentUser = () => {
-//   return JSON.parse(localStorage.getItem('token'));
-// };
-
-// const fetchProtectedData = async () => {
-//   try {
-//     const response = await axiosWithAuth().get('/protected-route');
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error fetching protected data:', error);
-//     throw error;
-//   }
-// };
-
-// const authService = {
-//   register,
-//   login,
-//   logout,
-//   getCurrentUser,
-//   fetchProtectedData,
-// };
-
-// export default authService;
-
-
 import axios from 'axios';
 
 const API_URL = 'http://localhost:8080/api/auth/';
@@ -93,19 +12,17 @@ const axiosWithAuth = () => {
   });
 };
 
-const register = async (firstName, lastName, username, email, phoneNumber,password) => {
+
+const register = async (userData) => {
   try {
-    const response = await axios.post(API_URL + 'register', {
-      firstName,
-      lastName,
-      username,
-      phoneNumber,
-      email,
-      password,
+    const response = await axios.post(API_URL + 'signup', userData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
     return response.data;
   } catch (error) {
-    console.error('Registration failed:', error);
+    console.error('Registration failed:', error.response?.data || error.message);
     throw error;
   }
 };
